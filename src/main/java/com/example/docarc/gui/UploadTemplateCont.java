6a4b2149b3abde270cf8332dba139533e2e4ec3c
Roman <yummison44@gmail.com>
2026-05-04 15:31:29 +0200

    temp

--- src/main/java/com/example/docarc/gui/UploadTemplateCont.java
+++ src/main/java/com/example/docarc/gui/UploadTemplateCont.java
@@ -1,4 +1,13 @@
 package com.example.docarc.gui;
 
+import com.example.docarc.be.ParentUser;
+import com.example.docarc.be.User;
+
 public class UploadTemplateCont {
+
+    private User user;
+
+    public void setUser(User usr){
+        this.user = usr;
+    }
 }
